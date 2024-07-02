3ae914297300c2465a65f39096bd1e07b5bae6b1
bylinskiynikiya796@gmail.com <bylinskiynikiya796@gmail.com>
2024-07-02 16:57:15 +0300

    final relize

--- app/src/main/java/com/example/mbanking/util/Variables.kt
+++ app/src/main/java/com/example/mbanking/util/Variables.kt
@@ -2,8 +2,8 @@ package com.example.mbanking.util
 
 import com.example.mbanking.data.AccountData
 
-var defaultStartDate = "01.01.1970"
-var defaultEndDate = "30.01.3000"
-var listOfAccounts = mutableListOf(AccountData.First, AccountData.Second, AccountData.Third)
+const val defaultStartDate = "01.01.1970"
+const val defaultEndDate = "30.01.3000"
+val listOfAccounts = mutableListOf(AccountData.First, AccountData.Second, AccountData.Third)
 var transactionIter = 0
 var accountValue: Int = 0
